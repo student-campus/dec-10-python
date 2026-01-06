f479ab313365099ac1185953df0e5f333b596fe2
student-campus <sakarki30@gmail.com>
2026-01-06 12:46:08 +0545

    dictionary

---
+++ dict.py
@@ -0,0 +1,13 @@
+info = {
+    "name": " Sirat",
+    "college": "prasadi",
+    "hobby": ["dance", "music", "reading"],
+    "marks": 99,
+}
+for i in info.keys():
+    print(i)
+
+for j in info.values():
+    print(j)
+
+print(info)
